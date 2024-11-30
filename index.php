5a42c33222115938cb1bb0ec8cc3cde233917dad
DESKTOP-IS16CSV\samirakhajavi <samirakh1381@gmail.com>
2024-11-30 15:11:40 +0330

    current url

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 include "system/config.php";
 include "system/rout.php";
-<<<<<<< HEAD
 include "app/controller/controller.php";
-=======
-include "app/controller.php";
->>>>>>> origin/main
+
+
